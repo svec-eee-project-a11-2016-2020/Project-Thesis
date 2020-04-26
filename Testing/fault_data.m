ec1d97c5f589b49bf4ba274ad3a1350707ca51c5
Pavankumar <epavankumar09@vidyanikethan.asia>
2020-04-26 09:56:17 +0530

    update

--- Testing/fault_data.m
+++ Testing/fault_data.m
@@ -1,5 +1,19 @@
+%---Fault Codes---
+% No-Fault = 0
+% ag       = 1
+% bg       = 2
+% cg       = 3
+% ab       = 4
+% bc       = 5
+% ca       = 6
+% ab-g     = 7
+% bc-g     = 8
+% ca-g     = 9
+% abc      = 10
+
+
 switch fault
-    case 0
+    case 0 
         x=1, abc = 0; cag = abc; bcg = cag; abg = bcg; ca = abg; bc = ca; ab = bc; cg = ab; bg = cg; ag = bg;
     case 1
         ag=1;
@@ -23,6 +37,6 @@ switch fault
     case 10
         abc=1; cag = 0; bcg = cag; abg = bcg; ca = abg; bc = ca; ab = bc; cg = ab; bg = cg; ag = bg; x = ag;
 end
-for i=[1:100]
+for i=[1:200]% for 200 samples of each fault
     faults(i,1:11)=[x,ag,bg,cg,ab,bc,ca,abg,bcg,cag,abc];
 end
